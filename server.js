0f7101d5f0b230dd2520a95f49d9e5084abe7d6a
kanphob <kanphob.k@gmail.com>
2026-03-10 21:46:08 +0700

    [WEJOY][0.0.1][Kan] Deploy New Version Allow CORS#2

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",        // allow all origins (for testing)
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("player connected");
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("server running");
+});
